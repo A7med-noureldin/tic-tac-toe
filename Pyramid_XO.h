9bcf9a7bfabf9ebb215a752b24cd4fa203f45954
A7med-noureldin <162290003+A7med-noureldin@users.noreply.github.com>
2024-12-02 15:39:36 +0200

    Add files via upload

---
+++ Pyramid_XO.h
@@ -0,0 +1,162 @@
+#ifndef GAMETEST_PYRAMID_XO_H
+#define GAMETEST_PYRAMID_XO_H
+
+#include "BoardGame_Classes.h"
+#include <bits/stdc++.h>
+
+using namespace std;
+
+template <typename T>
+class Board_XO: public Board<T> {
+public:
+    Board_XO();
+    bool update_board (int x , int y , T symbol);
+    void display_board ();
+    bool is_win();
+    bool is_draw();
+    bool game_is_over();
+};
+
+template <typename T>
+class Player_XO: public Player<T>{
+public:
+    Player_XO (string name, T symbol);
+    void getmove(int& x, int& y);
+};
+
+template <typename T>
+class RandomPlayer_XO : public RandomPlayer<T>{
+public:
+    RandomPlayer_XO (T symbol);
+    void getmove(int &x, int &y);
+};
+
+
+///////--------------------------------Implementation---------------------------------------///////
+///////----------------------Board Implementation----------------------///////
+template<typename T>
+Board_XO<T>::Board_XO() {
+    this->rows = 3;
+    this->columns = 5;
+    this->board = new char*[this->rows];
+    for (int i = 0; i < this->rows; i++) {
+        this->board[i] = new char[this->columns];
+        for (int j = 0; j < this->columns; j++) {
+            this->board[i][j] = 0;
+        }
+    }
+    this->n_moves = 0;
+}
+
+set<pair<int,int>> s = {{0,0}, {0,1}, {0,3}, {0,4}, {1,0}, {1,4}}; // Invalid positions
+
+template<typename T>
+bool Board_XO<T>::update_board(int x, int y, T symbol) {
+    if(!(x < 0 || x >= this->rows || y < 0 || y >= this->columns || s.count(make_pair(x, y))) && (this->board[x][y] == 0 || symbol == 0)){
+        if (symbol == 0){
+            this->n_moves--;
+            this->board[x][y] = 0;
+        }
+        else {
+            this->n_moves++;
+            this->board[x][y] = toupper(symbol);
+        }
+        return true;
+    }
+    return false;
+}
+
+template<typename T>
+void Board_XO<T>::display_board() {
+    bool good = true;
+    cout << setw(19);
+    for (int i = 0; i < 3; i++) {
+        if (i == 1)
+            cout << setw(10);
+        for (int j = 0; j < 5; j++) {
+            if(s.count(make_pair(i,j)))continue;
+            if(good){
+                cout << "|";
+                good = !good;
+            }
+            cout << '(' << i << ',' << j << ')' << setw(3) << this->board[i][j] << "|";
+        }
+        good = !good;
+        cout << endl;
+    }
+    cout << "----------------------------------------------------\n";
+}
+
+template<typename T>
+bool Board_XO<T>::game_is_over() {
+    return (is_win() || is_draw());
+}
+
+template<typename T>
+bool Board_XO<T>::is_draw() {
+    return (this->n_moves == 9 && !is_win());
+}
+
+template<typename T>
+bool Board_XO<T>::is_win() {
+    //check Rows
+    if(this->board[1][1] == this->board[1][2] && this->board[1][1] == this->board[1][3]){
+        return true;
+    }
+
+    if(this->board[2][0] == this->board[2][1] && this->board[2][0] == this->board[2][2]){
+        return true;
+    }
+
+    if(this->board[2][1] == this->board[2][2] && this->board[2][1] == this->board[2][3]){
+        return true;
+    }
+
+    if(this->board[2][2] == this->board[2][3] && this->board[2][2] == this->board[2][4]){
+        return true;
+    }
+
+    //check Column
+    if(this->board[2][2] == this->board[0][2] && this->board[2][2] == this->board[1][2]){
+        return true;
+    }
+
+    //check Diagonals
+    if(this->board[0][2] == this->board[1][3] && this->board[0][2] == this->board[2][4]){
+        return true;
+    }
+
+    if(this->board[0][2] == this->board[1][1] && this->board[0][2] == this->board[2][0]){
+        return true;
+    }
+
+    return false;
+}
+
+///////----------------------Player Implementation----------------------///////
+
+template<typename T>
+Player_XO<T>::Player_XO(string name, T symbol):Player<T>(name, symbol) {}
+
+template<typename T>
+void Player_XO<T>::getmove(int &x, int &y) {
+    cout << "\nPlease enter your move x and y (0 to 2) separated by spaces: ";
+    cin >> x >> y;
+}
+
+///////----------------------Random Player Implementation----------------------///////
+
+template<typename T>
+RandomPlayer_XO<T>::RandomPlayer_XO(T symbol):RandomPlayer<T>(symbol) {
+    this->dimension = 3;
+    this->name = "Random Computer Player";
+    srand(static_cast<unsigned int>(time(0)));
+}
+
+template<typename T>
+void RandomPlayer_XO<T>::getmove(int &x, int &y) {
+    x = rand() % this->dimension;  //Rows = 3
+    y = rand() % (this->dimension+2); // Columns = 5
+}
+
+#endif //GAMETEST_PYRAMID_XO_H
--- main.cpp
+++ main.cpp
@@ -1,14 +1,23 @@
-#include <iostream>
+#include <bits/stdc++.h>
 #include "BoardGame_Classes.h"
-#include "3x3X_O.h"
-
+#include "Pyramid_XO.h"
 
 using namespace std;
 
 int main() {
+    cout << "The board contains cells [ x , y ] , every cell has row number (x) and column number (y)\n";
+    cout << "so this is the board and each cell's information :\n";
+    cout << "                   -- [ 0 , 2 ] -- \n";
+    cout << "          - [ 1 , 1 ] [ 1 , 2 ] [ 1 , 3 ] - \n";
+    cout << "  [ 2 , 0 ] [ 2 , 1 ] [ 2 , 2 ] [ 2 , 3 ] [ 2 , 4 ] \n";
+    cout << "-----------------------------------------------------\n";
+    cout << "You have to enter the row's number first , then the column's number(space between 'EM). \n";
+    cout << "                    Have a good game! :)                         \n";
+    cout << endl;
+
     int choice;
     Player<char>* players[2];
-    X_O_Board<char>* B = new X_O_Board<char>();
+    Board_XO<char>* B = new Board_XO<char>();
     string playerXName, player2Name;
 
     cout << "Welcome to FCAI X-O Game. :)\n";
@@ -19,15 +28,20 @@ int main() {
     cout << "Choose Player X type:\n";
     cout << "1. Human\n";
     cout << "2. Random Computer\n";
+//    cout << "3. Smart Computer (AI)\n";
     cin >> choice;
 
     switch(choice) {
         case 1:
-            players[0] = new X_O_Player<char>(playerXName, 'X');
+            players[0] = new Player_XO<char>(playerXName, 'X');
             break;
         case 2:
-            players[0] = new X_O_Random_Player<char>('X');
+            players[0] = new RandomPlayer_XO<char>('X');
             break;
+//        case 3:
+//            players[0] = new X_O_MinMax_Player<char>('X');
+//            players[0]->setBoard(B);
+//            break;
         default:
             cout << "Invalid choice for Player 1. Exiting the game.\n";
             return 1;
@@ -39,15 +53,20 @@ int main() {
     cout << "Choose Player 2 type:\n";
     cout << "1. Human\n";
     cout << "2. Random Computer\n";
+//    cout << "3. Smart Computer (AI)\n";
     cin >> choice;
 
     switch(choice) {
         case 1:
-            players[1] = new X_O_Player<char>(player2Name, 'O');
+            players[1] = new Player_XO<char>(player2Name, 'O');
             break;
         case 2:
-            players[1] = new X_O_Random_Player<char>('O');
+            players[1] = new RandomPlayer_XO<char>('O');
             break;
+//        case 3:
+//            players[1] = new X_O_MinMax_Player<char>('O');
+//            players[1]->setBoard(B);
+//            break;
         default:
             cout << "Invalid choice for Player 2. Exiting the game.\n";
             return 1;
@@ -65,6 +84,3 @@ int main() {
 
     return 0;
 }
-
-
-
