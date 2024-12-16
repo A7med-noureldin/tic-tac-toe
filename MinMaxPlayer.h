b765978d881f557d7f4ed31f5e552d70af95874a
AhmedAbdellatif2 <162994885+AhmedAbdellatif2@users.noreply.github.com>
2024-12-16 19:25:10 +0200

    Add files via upload

--- MinMaxPlayer.h
+++ MinMaxPlayer.h
@@ -2,57 +2,53 @@
 #define _MINMAXPLAYER_H
 
 #include "BoardGame_Classes.h"
-
-template <typename T>
-class X_O_MinMax_Player : public Player<T> {
-public:
-    X_O_MinMax_Player(T symbol);
-
-    void getmove(int& x, int& y) override;
-
-private:
-    int calculateMinMax(T s, bool isMaximizing);
-    std::pair<int, int> getWorstMove();
-};
-
 #include <limits>
 #include <algorithm>
 using namespace std;
 
 template <typename T>
-X_O_MinMax_Player<T>::X_O_MinMax_Player(T symbol) : Player<T>(symbol) {
+class X_O_MinMax_Player : public Player<T> {
+public:
+    X_O_MinMax_Player(T symbol, Board<T>* board);  // Pass board pointer to constructor
+
+    void getmove(int& x, int& y) override;
+
+private:
+    int calculateMinMax(T s, bool isMaximizing, int depth);
+    pair<int, int> getBestMove();
+};
+
+template <typename T>
+X_O_MinMax_Player<T>::X_O_MinMax_Player(T symbol, Board<T>* board) : Player<T>(symbol) {
+    this->boardPtr = board;  // Initialize boardPtr with the passed board pointer
     this->name = "AI Player";
 }
 
 template <typename T>
 void X_O_MinMax_Player<T>::getmove(int& x, int& y) {
-    std::pair<int, int> worstMove = getWorstMove();
-    x = worstMove.first;
-    y = worstMove.second;
+    pair<int, int> bestMove = getBestMove();
+    x = bestMove.first;
+    y = bestMove.second;
 }
 
 template <typename T>
-int X_O_MinMax_Player<T>::calculateMinMax(T s, bool isMaximizing) {
+int X_O_MinMax_Player<T>::calculateMinMax(T s, bool isMaximizing, int depth) {
     if (this->boardPtr->is_win()) {
-        return isMaximizing ? -1 : 1;
+        return isMaximizing ? -10 + depth : 10 - depth; // Depth-based scoring
     } else if (this->boardPtr->is_draw()) {
         return 0;
     }
 
-    int bestValue = isMaximizing ? std::numeric_limits<int>::min() : std::numeric_limits<int>::max();
+    int bestValue = isMaximizing ? numeric_limits<int>::min() : numeric_limits<int>::max();
     T opponentSymbol = (s == 'X') ? 'O' : 'X';
 
     for (int i = 0; i < 3; ++i) {
         for (int j = 0; j < 3; ++j) {
             if (this->boardPtr->update_board(i, j, s)) {
-                int value = calculateMinMax(opponentSymbol, !isMaximizing);
+                int value = calculateMinMax(opponentSymbol, !isMaximizing, depth + 1);
                 this->boardPtr->update_board(i, j, 0);
 
-                if (isMaximizing) {
-                    bestValue = std::max(bestValue, value);
-                } else {
-                    bestValue = std::min(bestValue, value);
-                }
+                bestValue = isMaximizing ? max(bestValue, value) : min(bestValue, value);
             }
         }
     }
@@ -61,50 +57,25 @@ int X_O_MinMax_Player<T>::calculateMinMax(T s, bool isMaximizing) {
 }
 
 template <typename T>
-std::pair<int, int> X_O_MinMax_Player<T>::getWorstMove() {
-    int worstValue = std::numeric_limits<int>::max();
-    std::pair<int, int> worstMove = {-1, -1};
-    T opponentSymbol = (this->symbol == 'X') ? 'O' : 'X';
+pair<int, int> X_O_MinMax_Player<T>::getBestMove() {
+    int bestValue = numeric_limits<int>::min();
+    pair<int, int> bestMove = {-1, -1};
 
     for (int i = 0; i < 3; ++i) {
         for (int j = 0; j < 3; ++j) {
             if (this->boardPtr->update_board(i, j, this->symbol)) {
-                if (this->boardPtr->is_win()) {
-                    this->boardPtr->update_board(i, j, 0);
-                    continue;
-                }
-                this->boardPtr->update_board(i, j, 0);
-            }
-        }
-    }
-
-    for (int i = 0; i < 3; ++i) {
-        for (int j = 0; j < 3; ++j) {
-            if (this->boardPtr->update_board(i, j, opponentSymbol)) {
-                if (this->boardPtr->is_win()) {
-                    this->boardPtr->update_board(i, j, 0);
-                    continue;
-                }
-                this->boardPtr->update_board(i, j, 0);
-            }
-        }
-    }
-
-    for (int i = 0; i < 3; ++i) {
-        for (int j = 0; j < 3; ++j) {
-            if (this->boardPtr->update_board(i, j, this->symbol)) {
-                int moveValue = calculateMinMax(this->symbol, false);
+                int moveValue = calculateMinMax(this->symbol, false, 0);
                 this->boardPtr->update_board(i, j, 0);
 
-                if (moveValue < worstValue) {
-                    worstMove = {i, j};
-                    worstValue = moveValue;
+                if (moveValue > bestValue) {
+                    bestMove = {i, j};
+                    bestValue = moveValue;
                 }
             }
         }
     }
 
-    return worstMove;
+    return bestMove;
 }
 
 #endif //_MINMAXPLAYER_H
